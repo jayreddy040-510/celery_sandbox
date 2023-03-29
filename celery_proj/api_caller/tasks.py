bdae4e3bcdf584f0741df38c91cb812ccf408bd9
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-03-28 19:06:57 -0700

    working server

--- celery_proj/api_caller/tasks.py
+++ celery_proj/api_caller/tasks.py
@@ -1,7 +1,9 @@
 from celery import shared_task
+import time
 
 @shared_task(bind=True)
 def test(self):
-    for in in range(10):
+    for i in range(10):
         print(i)
+        time.sleep(2)
     return "done"
--- celery_proj/api_caller/views.py
+++ celery_proj/api_caller/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .tasks import test
+from django.http import HttpResponse
 
 def test_view(request):
-    test.delay()
-    return HttpResponse("http request done")
+    print("inside the view")
+    if request.method == 'POST':
+        test.delay()
+        return HttpResponse("http request done")
