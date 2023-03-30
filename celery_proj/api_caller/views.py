53ffb37079a577cd2a400c31e72c39c885085ec7
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-03-30 00:13:01 -0700

    chain successfully apply_async(), needs to fix apply_async()

--- celery_proj/api_caller/views.py
+++ celery_proj/api_caller/views.py
@@ -14,5 +14,5 @@ def test_sleep_view(request):
         task_chain = chain(
             sleeper_task.s(num) for num in range(4)        
                 )
-        task_chain.apply_async
+        task_chain.apply_async()
         return HttpResponse("ugh let me sleep")
--- celery_proj/celery_proj/celery.py
+++ celery_proj/celery_proj/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 import os
+
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_sandbox.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_proj.settings')
 
-app = Celery('celery_sandbox')
+app = Celery('celery_proj')
 app.conf.enable_utc = False
 
 app.conf.update(timezone = 'America/Los_Angeles')
