5908c8fc9cd9049f4fbb4bafb3be8f405a051b5c
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-03-30 21:02:13 -0700

    adds.si() method shared_task so that doesn't require bind=True

--- celery_proj/api_caller/views.py
+++ celery_proj/api_caller/views.py
@@ -12,7 +12,7 @@ def test_view(request):
 def test_sleep_view(request):
     if request.method == 'POST':
         task_chain = chain(
-            sleeper_task.s(num) for num in range(4)        
+            sleeper_task.si(num) for num in range(4)        
                 )
         task_chain.apply_async()
         return HttpResponse("ugh let me sleep")
